2a0710e7cc0193c7ab5eb21349e6e889729d3e78
KENNE FOKEM Boris <Boris.KENNE-FOKEM@akkodis.com>
2024-09-11 13:36:59 +0100

    Add the model Genre , that will be use to predefine the posible values for the attribute genre

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 class Band(models.Model):
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR
+        
     name = models.fields.CharField(max_length=100)
     genre = models.fields.CharField(max_length=50)
     biography = models.fields.CharField(max_length=1000)
