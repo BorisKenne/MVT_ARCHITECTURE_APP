05a51a89624e95050376238fa8d254fdda7c5327
KENNE FOKEM Boris <Boris.KENNE-FOKEM@akkodis.com>
2024-09-11 14:29:58 +0100

    Add the model Listing ,makemigration and migrate

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -12,3 +12,15 @@ class Band(models.Model):
     year_formed = models.fields.IntegerField(validators=[MinValueValidator(1900),MaxValueValidator(2021)])
     active = models.fields.BooleanField(default=True)
     official_homepage = models.fields.URLField(null=True,blank=True)
+
+class Listing(models.Model):
+    class Types(models.TextChoices):
+        Records ='Records'
+        Clothing ='Clothing'
+        Posters = 'Posters'
+        Miscellaneous = 'Miscellaneous'
+
+    description = models.fields.CharField(max_length=500)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(null=True)
+    Type = models.fields.CharField(choices=Types.choices,max_length=15)
