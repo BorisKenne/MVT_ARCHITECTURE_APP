1047ef4426cf7c9c8b4d109f6ec16d98c3674a72
KENNE FOKEM Boris <Boris.KENNE-FOKEM@akkodis.com>
2024-09-13 09:41:48 +0100

    Create the form  BandForm

--- merchex/listings/forms.py
+++ merchex/listings/forms.py
@@ -4,3 +4,9 @@ class ContactUsForm(forms.Form):
    name = forms.CharField(required=False)
    email = forms.EmailField()
    message = forms.CharField(max_length=1000)
+
+class BandForm(forms.Form):
+   name = forms.CharField(max_length=100)
+   biography = forms.CharField(max_length=1000)
+   year_formed = forms.IntegerField(min_value=1900, max_value=2021)
+   official_homepage = forms.URLField(required=False)
