2a4e63e34eff51e7b46fc85c42cc4d63db08f5f0
KENNE FOKEM Boris <Boris.KENNE-FOKEM@akkodis.com>
2024-09-13 11:14:07 +0100

    Add the process to delete a band

--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('bands/<int:id>/', views.band_detail, name='band-detail'),
     path('bands/add/', views.band_create, name='band-create'),
     path('bands/<int:id>/change', views.band_update, name='band-update'),
+    path('bands/<int:id>/delete', views.band_delete, name='band-delete'),
     path('about-us/', views.about,name='about'),
     path('contact-us/', views.contact,name='contact'),
     path('listings/', views.listing_list,name='listing-list'),
